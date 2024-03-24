7fe90122aded29605da612fa0431fe097114a2ca
s28215 <s28215@pjwstk.edu.pl>
2024-03-24 22:40:36 +0100

    Task 2 done

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 squares = [x**2 for x in range(1, 11)]
 print("List of squares of numbers from 1 to 10:", squares)
+def generate_squares(start, end):
+    squares = [x**2 for x in range(start, end + 1)]
+    return squares
+start = 1
+end = 10
+squares = generate_squares(start, end)
+print("List of squares of numbers from", start, "to", end, ":", squares)
 
