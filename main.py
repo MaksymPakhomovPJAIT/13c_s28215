0388d36b2e640ca7111f514ffed5f1ebba0d4551
s28215 <s28215@pjwstk.edu.pl>
2024-03-24 22:41:35 +0100

    Task 3 done

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ end = 10
 squares = generate_squares(start, end)
 print("List of squares of numbers from", start, "to", end, ":", squares)
 
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
+square_gen = SquareGenerator()
+start = 1
+end = 10
+squares = square_gen.generate_squares(start, end)
+print("List of squares of numbers from", start, "to", end, ":", squares)
+
